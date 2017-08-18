9671773a3c233c28277e0c21d24a0a9e36c3619d
snehal2495 <vichare.snehal24@gmail.com>
2017-08-18 16:53:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/blee/courtcounter/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.blee.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    int teamScoreA=0;
+    int teamScoreB=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void addThreePoints(View view){
+        teamScoreA=teamScoreA+3;
+        scoreforTeamA(teamScoreA);
+
+    }
+
+    public void addThreePointsB(View view){
+        teamScoreB=teamScoreB+3;
+        scoreForTeamB(teamScoreB);
+
+    }
+
+
+    public void addTwoPoints(View view){
+        teamScoreA=teamScoreA+2;
+        scoreforTeamA(teamScoreA);
+    }
+
+    public void addTwoPointsB(View view){
+        teamScoreB=teamScoreB+2;
+        scoreForTeamB(teamScoreB);
+    }
+
+    public void addOnePoints(View view){
+        teamScoreA=teamScoreA+1;
+        scoreforTeamA(teamScoreA);
+
+    }
+    public void addOnePointsB(View view){
+       teamScoreB=teamScoreB+1;
+        scoreForTeamB(teamScoreB);
+
+    }
+
+
+
+    public void scoreforTeamA(int score)
+    {
+        TextView scoreText=(TextView) findViewById(R.id.textview2);
+        scoreText.setText(String.valueOf(score));
+
+
+    }
+
+    public void scoreForTeamB(int scoreB)
+    {
+        TextView scoreb=(TextView)findViewById(R.id.textview4);
+        scoreb.setText(String.valueOf(scoreB));
+    }
+
+    public void resetScore(View view)
+    {
+        teamScoreA=0;
+        teamScoreB=0;
+        scoreforTeamA(teamScoreA);
+        scoreForTeamB(teamScoreB);
+    }
+
+
+}
